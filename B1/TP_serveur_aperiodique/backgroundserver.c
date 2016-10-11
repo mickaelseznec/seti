085bde80281adc30e4f689b0e8ce46cae48adee2
Mickaël Seznec <mickael.seznec@netatmo.com>
2016-10-11 23:09:22 +0200

    Initial commit for TP_serveur_aperiodique

---
+++ B1/TP_serveur_aperiodique/backgroundserver.c
@@ -0,0 +1,47 @@
+#include "events.h"
+#include "periodictasks.h"
+#include "tasks.h"
+
+// Main procedure of background server
+
+void *runBackgroundServer (void *t){
+  TaskInfo           s;
+  EventInfo          e;
+  Time               d = 0;
+
+  s = periodicTaskTable[*(int *)t];
+
+  while (systemNotCompleted() && (0 <= lastEvent)) {
+    e = getEvent(firstEvent);
+    
+    // Wait for event activation
+    NYI("wait for event activation");
+
+    putHeader(s.name);
+    putString(s.name);
+    putString("=+oo ");
+    putString(e.name);
+    putString("=");
+    putTime  (e.computation);
+    newLine();
+
+    // Remove first event from event queue
+    NYI("remove event and wait for activation");
+
+    // Simulate the execution of this event using
+    // computeDuringTimeSpan. Provide the name of the event, its worst
+    // case execution time, and the period of the server. ATTENTION :
+    // the period parameter in computeDuringTimeSPan is used to
+    // compute the execution priority. See in tasks.h.
+    NYI("compute event");
+
+    // Print event completion
+    putHeader(s.name);
+    putString("C=oo ");
+    putString(e.name);
+    putString(" completed");
+    newLine();
+  }
+
+  return NULL;
+}
